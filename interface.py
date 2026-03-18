469da4630eaa9698638411f8b032019ffe7a4e4c
Omc-1 <2503230196@cesunbc.edu.mx>
2026-03-17 21:10:16 -0700

    Delete interface.py

--- interface.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Dict, Any
-
-class ISerializable(ABC):
-    """Interfaz que obliga a las clases a convertirse en diccionario y guardarse en un JSON."""
-
-    @abstractmethod
-    def to_dict(self) -> Dict[str, Any]:
-        pass
